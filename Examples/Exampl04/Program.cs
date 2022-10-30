66bdb9eb9e1561f926efb7c2b7ca5d8d48bdf8de
Olga Rybakova <olgarybka@mail.ru>
2022-10-31 04:24:27 +0500

    readme.md коррект.ссылок задача 8, и сама задача

---
+++ Examples/Exampl04/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите целое число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+string raz = " -> ";
+int index = 1;
+Console.Write(N);
+while (index <= N)
+{
+    if (index % 2 == 0)
+    {
+        Console.Write(raz + index);
+        raz = ", ";
+    }
+    index = index + 1;
+}
