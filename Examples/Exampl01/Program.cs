104e38a795593e3be92cd7e925fbdb37fda1575c
Olga Rybakova <olgarybka@mail.ru>
2022-10-31 02:51:57 +0500

    Ura сделан 2 пример

--- Examples/Exampl01/Program.cs
+++ Examples/Exampl01/Program.cs
@@ -1,5 +1,5 @@
-﻿int a = 5;
-int b = 7;
+﻿int a = -9;
+int b = -3;
 int max = a;
 if (max < b)
 {
--- Examples/Exampl02/Program.cs
+++ Examples/Exampl02/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int num1=44, num2=5, num3=78;
+Console.WriteLine("Из трех чисел: ", num1+" , "+num2+" , "+num3+" найти максимальное");
+int max=num1;
+if (max < num2)
+{
+    max=num2;
+}
+if (max < num3)
+{
+    max=num3;
+}
+Console.WriteLine(num1+" , "+num2+" , "+num3+" -> "+max);
