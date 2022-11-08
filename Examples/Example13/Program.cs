6294689e9d013539ee0cc04c2f6469c4f3c67b76
Olga Rybakova <olgarybka@mail.ru>
2022-11-08 21:01:06 +0500

    Создана задача 25

---
+++ Examples/Example13/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача 25: 
+//_Напишите метод, который принимает на вход два числа (A и B) и возводит число A в целую степень B._
+
+//3, 5 -> 243 (3⁵)
+//
+//2, 4 -> 16
+
+//без использования math.pow
+double pow(int A, int B)
+{
+    if (A == 0 && B == 0) return double.NaN;
+    bool flag = B>0;
+    if (B < 0)  B = -B;
+    int res = 1;
+    for (int i = 0; i < B; i++)  res = res * A;
+      if (flag) return res;
+    else return 1.0 / res;
+}
+
+Console.WriteLine("Введите первое целое  число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе целое число, степень в которую возвести первое число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+double Power = pow(num1, num2);
+Console.Write($"{num1} в степени {num2} -> {Power}");
